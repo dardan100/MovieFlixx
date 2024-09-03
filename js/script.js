615b352603593da6e4d40b671ddcfccf635b995f
dardan100 <nuredinidardan5@gmail.com>
2024-09-03 12:00:46 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -53,36 +53,68 @@ async function displayPopularMovies() {
 }
 
 // Display 20 most popular tv shows
+// async function displayPopularShows() {
+//   const { results } = await fetchAPIData("tv/popular");
+
+//   results.forEach((show) => {
+//     const div = document.createElement("div");
+//     div.classList.add("card");
+//     div.innerHTML = `
+//           <a href="tv-details.html?id=${show.id}">
+//             ${
+//               show.poster_path
+//                 ? `<img
+//               src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+//               class="card-img-top"
+//               alt="${show.name}"
+//             />`
+//                 : `<img
+//             src="../images/no-image.jpg"
+//             class="card-img-top"
+//             alt="${show.name}"
+//           />`
+//             }
+//           </a>
+//           <div class="card-body">
+//             <h5 class="card-title">${show.name}</h5>
+//             <p class="card-text">
+//               <small class="text-muted">Air Date: ${show.first_air_date}</small>
+//             </p>
+//           </div>
+//         `;
+
+//     document.querySelector("#popular-shows").appendChild(div);
+//   });
+// }
+
 async function displayPopularShows() {
+  console.log("Fetching popular shows...");
   const { results } = await fetchAPIData("tv/popular");
+  console.log("Fetched shows:", results);
+
+  if (!results) {
+    console.error("No results found");
+    return;
+  }
 
   results.forEach((show) => {
     const div = document.createElement("div");
     div.classList.add("card");
     div.innerHTML = `
-          <a href="tv-details.html?id=${show.id}">
-            ${
-              show.poster_path
-                ? `<img
-              src="https://image.tmdb.org/t/p/w500${show.poster_path}"
-              class="card-img-top"
-              alt="${show.name}"
-            />`
-                : `<img
-            src="../images/no-image.jpg"
-            class="card-img-top"
-            alt="${show.name}"
-          />`
-            }
-          </a>
-          <div class="card-body">
-            <h5 class="card-title">${show.name}</h5>
-            <p class="card-text">
-              <small class="text-muted">Air Date: ${show.first_air_date}</small>
-            </p>
-          </div>
-        `;
-
+      <a href="tv-details.html?id=${show.id}">
+        ${
+          show.poster_path
+            ? `<img src="https://image.tmdb.org/t/p/w500${show.poster_path}" class="card-img-top" alt="${show.name}" />`
+            : `<img src="../images/no-image.jpg" class="card-img-top" alt="${show.name}" />`
+        }
+      </a>
+      <div class="card-body">
+        <h5 class="card-title">${show.name}</h5>
+        <p class="card-text">
+          <small class="text-muted">Air Date: ${show.first_air_date}</small>
+        </p>
+      </div>
+    `;
     document.querySelector("#popular-shows").appendChild(div);
   });
 }
